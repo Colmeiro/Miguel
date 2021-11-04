a3a9a575bff649c831199bcde6dbd3e356320251
Miguel Colmeiro <colmeiro@yahoo.es>
2021-11-04 16:40:22 +0100

    v4_vistas_CRD

--- application/modules/privado/controllers/Contacto.php
+++ application/modules/privado/controllers/Contacto.php
@@ -176,7 +176,7 @@ class Contacto extends MX_Controller
                 // 'orden' => set_value('orden'),
             )
         );
-        $this->db->order_by('contacto_id', 'ASC');
+        // $this->db->order_by('contacto_id', 'ASC');
         // $data['s_rol_id'] = $this->db->get('rol')->result();
         // $data['seccion'] = 'admin-users';
         $data['main'] = 'contacto_form';
@@ -187,33 +187,59 @@ class Contacto extends MX_Controller
     }
 
 
+    // public function create_action()
+    // {
+    //     $this->_rules('create');
+
+    //     if ($this->form_validation->run() == FALSE) {
+    //         $this->create();
+    //     } else {
+    //         $data = array(
+    //             // 'fecha_creacion' => date('Y-m-d H:i:s'),
+    //             'contacto_nombre' => $this->input->post('contacto_nombre', TRUE),
+    //             'contacto_telefono' => $this->input->post('contacto_telefono', TRUE),
+    //             'contacto_activo' => $this->input->post('contacto_activo', TRUE),
+    //             // 'apellidos' => $this->input->post('apellidos', TRUE),
+    //             // // 'dni' => $this->input->post('dni', TRUE),
+    //             // // 'ciudad' => $this->input->post('ciudad', TRUE),
+    //             // 'email' => $this->input->post('email', TRUE),
+    //             // 'password' => md5($this->input->post('password', TRUE)),
+    //             // 'rol_id' => $this->input->post('rol_id', TRUE),
+    //             // 'contacto_activo' => $this->input->post('activo', TRUE) ? $this->input->post('activo', TRUE) : 0,
+    //             // 'orden' => $this->input->post('orden', TRUE),
+    //         );
+
+    //         $this->MContacto_priv->insert($data);
+    //         $this->session->set_flashdata('message', 'contacto creado correctamente');
+    //         redirect(site_url('privado/contacto'));
+    //     }
+    // }
+
+
     public function create_action()
     {
-        $this->_rules('create');
+        $data = array(
+        // 'fecha_creacion' => date('Y-m-d H:i:s'),
+        'contacto_nombre' => $this->input->post('contacto_nombre', TRUE),
+        'contacto_telefono' => $this->input->post('contacto_telefono', TRUE),
+        'contacto_activo' => $this->input->post('contacto_activo', TRUE),
+        // 'apellidos' => $this->input->post('apellidos', TRUE),
+        // // 'dni' => $this->input->post('dni', TRUE),
+        // // 'ciudad' => $this->input->post('ciudad', TRUE),
+        // 'email' => $this->input->post('email', TRUE),
+        // 'password' => md5($this->input->post('password', TRUE)),
+        // 'rol_id' => $this->input->post('rol_id', TRUE),
+        // 'contacto_activo' => $this->input->post('activo', TRUE) ? $this->input->post('activo', TRUE) : 0,
+        // 'orden' => $this->input->post('orden', TRUE),
+        );
 
-        if ($this->form_validation->run() == FALSE) {
-            $this->create();
-        } else {
-            $data = array(
-                // 'fecha_creacion' => date('Y-m-d H:i:s'),
-                'contacto_nombre' => $this->input->post('contacto_nombre', TRUE),
-                'contacto_telefono' => $this->input->post('contacto_telefono', TRUE),
-                'contacto_activo' => $this->input->post('contacto_activo', TRUE),
-                // 'apellidos' => $this->input->post('apellidos', TRUE),
-                // // 'dni' => $this->input->post('dni', TRUE),
-                // // 'ciudad' => $this->input->post('ciudad', TRUE),
-                // 'email' => $this->input->post('email', TRUE),
-                // 'password' => md5($this->input->post('password', TRUE)),
-                // 'rol_id' => $this->input->post('rol_id', TRUE),
-                // 'contacto_activo' => $this->input->post('activo', TRUE) ? $this->input->post('activo', TRUE) : 0,
-                // 'orden' => $this->input->post('orden', TRUE),
-            );
-
-            $this->MContacto_priv->insert($data);
-            $this->session->set_flashdata('message', 'contacto creado correctamente');
-            redirect(site_url('privado/contacto'));
-        }
+        $this->MContacto_priv->insert($data);
+        $this->session->set_flashdata('message', 'contacto creado correctamente');
+        redirect(site_url('privado/contacto'));
     }
+    
+
+
 
 
     public function update($id)
--- application/modules/privado/views/contacto_form.php
+++ application/modules/privado/views/contacto_form.php
@@ -54,9 +54,7 @@
                     </div>
 
                     <div class="col-12">
-                        <?/*
-                        <h4 class="content-header-title float-left no-border mb-0 text-capitalize"><i class="bx bx-group"></i><?= $titulo ?></h4>
-                        */?>
+                        <h4 class="content-header-title float-left no-border mb-0 text-capitalize"><i class="bx bx-group"></i><?= $subtitulo ?></h4>
                     </div>
                 </div>
             </div>
@@ -81,107 +79,72 @@
                                 $bg_card="bg-light-green";
                                 $dots=": ";
                             }?>
+
                             <div class="card-header <?php $bg_card?> mb-2">
                                 <h4 class="card-title"><?php isset($subtitulo) ? $subtitulo : '' ?><?php $dots;?> <?php echo daFormato($data_fields['contacto_nombre'], 'varchar', '0-#475F7B', '') ?> </h4>
                             </div>
                             
                             <div class="card-content">
                                 <div class="card-body card-body-xs">
-                                    <? if (isset($data_fields)) extract($data_fields); //Provisional 
+                                    <?php if (isset($data_fields)) extract($data_fields); //Provisional 
                                     ?>
 
                                     <form class="form form-vertical form-edit" action="<?php echo $action; ?>" method="post">
                                         <div class="form-body">
                                             <div class="row">
                                                 <div class="col-12">
-                                                    <div class="form-group<?= form_error('nombre') != '' ? ' has-error' : '' ?>">
-                                                        <label for="nombre">Nombre <?php echo form_error('nombre') ?></label>
-                                                        <?= daFormatoEdit($data_fields['contacto_nombre'], 'nombre', 'Nombre', 'varchar', 'varchar', 1); ?>
-                                                        <? if (form_error('nombre') != '') { ?> <span class="help-block"><?= form_error('nombre') ?></span> <? } ?>
+                                                    <div class="form-group<?= form_error('contacto_nombre') != '' ? ' has-error' : '' ?>">
+                                                        <label for="contacto_nombre">Nombre <?php echo form_error('contacto_nombre') ?></label>
+                                                        <?= daFormatoEdit($data_fields['contacto_nombre'], 'contacto_nombre', 'Nombre', 'varchar', 'varchar', 1); ?>
+                                                        <? if (form_error('contacto_nombre') != '') { ?> <span class="help-block"><?= form_error('contacto_nombre') ?></span> <? } ?>
                                                     </div>
                                                 </div>
                                                 <div class="col-12">
                                                     <div class="form-group<?= form_error('telefono') != '' ? ' has-error' : '' ?>">
-                                                        <label for="apellidos">Teléfono <?php echo form_error('telefono') ?></label>
-                                                        <?= daFormatoEdit($data_fields['contacto_telefono'], 'telefono', 'Teléfono', 'varchar', 'varchar', 1); ?>
-                                                        <? if (form_error('telefono') != '') { ?> <span class="help-block"><?= form_error('telefono') ?></span> <? } ?>
+                                                        <label for="contacto_telefono">Teléfono <?php echo form_error('contacto_telefono') ?></label>
+                                                        <?= daFormatoEdit($data_fields['contacto_telefono'], 'contacto_telefono', 'Teléfono', 'varchar', 'varchar', 1); ?>
+                                                        <? if (form_error('contacto_telefono') != '') { ?> <span class="help-block"><?= form_error('contacto_telefono') ?></span> <? } ?>
                                                     </div>
                                                 </div>
-                                                <!-- <?/*<div class="col-12">
-                                                    <div class="form-group<?= form_error('dni') != '' ? ' has-error' : '' ?>">
-                                                        <label for="dni">DNI <?php echo form_error('dni') ?></label>
-                                                        <?= daFormatoEdit($data_fields['dni'], 'dni', 'Dni', 'varchar', 'varchar', 1); ?>
-                                                        <? if (form_error('dni') != '') { ?> <span class="help-block"><?= form_error('dni') ?></span> <? } ?>
-                                                    </div>
-                                                </div>
-                                                <div class="col-12">
-                                                    <div class="form-group<?= form_error('ciudad') != '' ? ' has-error' : '' ?>">
-                                                        <label for="ciudad">Ciudad <?php echo form_error('ciudad') ?></label>
-                                                        <?= daFormatoEdit($data_fields['ciudad'], 'ciudad', 'Ciudad', 'varchar', 'varchar', 1); ?>
-                                                        <? if (form_error('ciudad') != '') { ?> <span class="help-block"><?= form_error('ciudad') ?></span> <? } ?>
-                                                    </div>
-                                                </div>*/?> -->
-                                                <!-- <div class="col-12">
-                                                    <div class="form-group<?= form_error('email') != '' ? ' has-error' : '' ?>">
-                                                        <label for="email">Email <?php echo form_error('email') ?></label>
-                                                        <?= daFormatoEdit($data_fields['email'], 'email', 'Email', 'varchar', 'email', 1); ?>
-                                                        <? if (form_error('email') != '') { ?> <span class="help-block"><?= form_error('email') ?></span> <? } ?>
-                                                    </div>
-                                                </div> -->
-                                                <?
-                                                if (strpos($action, 'create_action') !== FALSE) {
-                                                ?>
-                                                    <!-- <div class="col-12">
-                                                        <div class="form-group<?= form_error('password') != '' ? ' has-error' : '' ?>">
-                                                            <label for="password">Password <?php echo form_error('password') ?></label>
-                                                            <?= daFormatoEdit($data_fields['password'], 'password', 'Password', 'varchar', 'password', 1); ?>
-                                                            <? if (form_error('password') != '') { ?> <span class="help-block"><?= form_error('password') ?></span> <? } ?>
-                                                        </div>
-                                                    </div> -->
-                                                <?
-                                                }
-                                                ?>
-                                                <!-- <div class="col-12">
-                                                    <div class="form-group<?= form_error('rol_id') != '' ? ' has-error' : '' ?>">
-                                                        <label for="rol_id">Rol <?php echo form_error('rol_id') ?></label>
-                                                        <select class="form-control" name="rol_id" id="rol_id" required><? foreach ($s_rol_id as $c) {
-                                                                                                                        ?>
-                                                                <option value="<?= $c->rol_id ?>" <?= $c->rol_id == $data_fields['rol_id'] ? 'selected="selected"' : '' ?>><?= $c->nombre ?></option>
-                                                            <?
-                                                                                                                        } ?></select>
-                                                        <? if (form_error('rol_id') != '') { ?> <span class="help-block"><?= form_error('rol_id') ?></span> <? } ?>
-                                                    </div> -->
-                                                </div>
-                                                <div class="col-12">
-                                                    <div class="form-group<?= form_error('activo') != '' ? ' has-error' : '' ?>">
-                                                        <label for="activo">Activo <?php echo form_error('activo') ?></label>
 
-                                                        <div class="checkbox checkbox-primary ml-1">
-                                                        <?= daFormatoEdit($data_fields['contacto_activo'], 'activo', 'Activo', 'tinyint', 'checkbox', 0); ?>
+                                                </div>
+                                                <div class="col-12">
+                                                    <div class="form-group<?php form_error('contacto_activo') != '' ? ' has-error' : '' ?>">
+                                                        <label for="contacto_activo">Activo <?php echo form_error('contacto_activo') ?></label>
+
+                                                        <!-- <div class="checkbox checkbox-primary ml-1">
+                                                            <label for="contacto_activo"></label>
+                                                            <?php daFormatoEdit($data_fields['contacto_activo'], 'contacto_activo', 'Activo', 'tinyint', 'checkbox', 0); ?>
+                                                        </div> -->
+
+                                                        <input type="checkbox" class="checkbox checkbox-primary ml-1" id="contacto_activo" checked> 
                                                         <label for="contacto_activo"></label>
-                                                        </div>
+                                                        <?php daFormatoEdit($data_fields['contacto_activo'], 'contacto_activo', 'Activo', 'tinyint', 'checkbox', 0); ?>
                                                         
-                                                        <? if (form_error('contacto_activo') != '') { ?> <span class="help-block"><?= form_error('contacto_activo') ?></span> <? } ?>
+                                                        <?php if (form_error('contacto_activo') != '') { ?> <span class="help-block"><?php form_error('contacto_activo') ?></span> <?php } ?>
                                                     </div>
                                                 </div>
-                                                <!-- <?/*<div class="col-12">
-                                                    <div class="form-group<?= form_error('orden') != '' ? ' has-error' : '' ?>">
-                                                        <label for="orden">Orden <?php echo form_error('orden') ?></label>
-                                                        <?= daFormatoEdit($data_fields['orden'], 'orden', 'Orden', 'int', 'int', 0); ?>
-                                                        <? if (form_error('orden') != '') { ?> <span class="help-block"><?= form_error('orden') ?></span> <? } ?>
-                                                    </div>
-                                                </div>*/?> -->
 
                                                 <input type="hidden" name="contacto_id" value="<?php echo $data_fields['contacto_id']; ?>" />
 
+                                                <div class="col-6 d-flex" style="text-align:left;">
 
+                                                <button type="submit" action="<?php echo site_url('privado/contacto/create_action'); ?> method="post" class="btn btn-primary"><?php echo $button ?></button>
+
+                                                    <!-- <?php if (isset($subtitulo) && $subtitulo==='Añadir Contacto') { ?>
+                                                            <button type="submit" action="<?php echo site_url('privado/contacto/create'); ?> method="post" class="btn btn-primary"><?php echo $button ?></button>
+                                                            
+
+                                                            <?php } else { ?>
+                                                                <button type="submit" action="<?php echo site_url('privado/contacto/update' . $data_fields['contacto_id']); ?>" method="post" class="btn btn-primary"><?php echo $button ?></button>
+                                                                <?php echo "hey"; ?>
+                                                                
+                                                    <?php }; ?> -->
+                                                </div>
                                                 
-
-                                                    <div class="col-6 d-flex" style="text-align:left;">
-                                                        <button type="submit" action="<?php echo site_url('privado/contacto/update' . $data_fields['contacto_id']); ?>" method="post" class="btn btn-primary"><?php echo $button ?></button>
-                                                    </div>
-
+                                                    
                                             </div>
+                                            </form>
                                         </div>
                                     </form>
                                 </div>
